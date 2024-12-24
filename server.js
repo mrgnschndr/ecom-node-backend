d538fc21ab2af20af26046620e3099145a13beee
mrgnschndr <mrgnschndr@gmail.com>
2024-12-23 18:41:12 -0600

    first draft of faker loop to seed 100 users in ecommerce db

--- server.js
+++ server.js
@@ -11,3 +11,27 @@ const { faker } = require("@faker-js/faker");
 app.listen(process.env.PORT, () => {
     console.log(`Server running on port ${PORT}`);
 })
+
+app.get('/users', (req, res) => {
+    let userArray = [];
+    for (let i = 0; i < 100; i++) {
+        userArray[i] = {
+            name_first: faker.person.firstName(),
+            name_last: faker.person.lastName(),
+            email: faker.internet.email({ firstName: name_first, lastName: name_last }),
+            password: faker.internet.password(),
+            phone_number: faker.phone.number(),
+            date_of_birth: faker.date.birthdate(),
+            address: faker.location.streetAddress(),
+            city: faker.location.city(),
+            state: faker.location.state(),
+            country: faker.location.country(),
+            postal_code: faker.location.zipCode(),
+            role: faker.person.jobTitle(),
+            profile_picture_url: faker.image.avatar(),
+            created_at: faker.date.past(),
+            updated_at: faker.date.recent()
+        }
+    }
+    res.json(userArray);
+})
